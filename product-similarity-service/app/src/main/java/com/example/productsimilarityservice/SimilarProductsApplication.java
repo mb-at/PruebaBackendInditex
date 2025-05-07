72e95c68251ccf14ad0325c7883fb21252b8167f
mb-at <mbat86400@gmail.com>
2025-05-07 20:33:10 +0200

    chore: initial scaffold with .gitignore and hexagonal layout

---
+++ product-similarity-service/app/src/main/java/com/example/productsimilarityservice/SimilarProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.productsimilarityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSimilarityServiceApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSimilarityServiceApplication .class, args);
+	}
+
+}
---
+++ product-similarity-service/app/src/test/java/com/example/similarbackend/SimilarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.similarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
