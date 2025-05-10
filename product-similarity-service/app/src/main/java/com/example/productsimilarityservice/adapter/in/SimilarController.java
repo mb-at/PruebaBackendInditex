9ed890e3cefced5fcdd5121226bd0f08a2a1ff87
mb-at <mbat86400@gmail.com>
2025-05-10 13:01:37 +0200

    feat(controller): add doc of '/{id}/similar' endpoint

--- product-similarity-service/app/src/main/java/com/example/productsimilarityservice/adapter/in/SimilarController.java
+++ product-similarity-service/app/src/main/java/com/example/productsimilarityservice/adapter/in/SimilarController.java
@@ -1,11 +1,18 @@
 package com.example.productsimilarityservice.adapter.in;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.productsimilarityservice.domain.model.Product;
 import com.example.productsimilarityservice.domain.port.in.GetSimilarProducts;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping("/product")
@@ -17,6 +24,12 @@ public class SimilarController {
         this.getSimilarProducts = getSimilarProducts;
     }
 
+    @Operation(
+        summary = "Obtener productos similares a un producto pasado por parámetro",
+        responses = {
+        @ApiResponse(responseCode = "200", description = "Listado de productos similares")
+        }
+    )
     @GetMapping("/{id}/similar")
     public ResponseEntity<List<Product>> getSimilar(@PathVariable("id") String productId) {
         List<Product> similars = getSimilarProducts.execute(productId);
