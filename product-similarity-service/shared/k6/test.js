263bcbab4d6f07ae2ac0bb5a67daaaa1232750a5
mb-at <mbat86400@gmail.com>
2025-05-11 02:02:32 +0200

    chore(config): Docker, yaml and test changes and implementations to include app in docker compose up

--- product-similarity-service/shared/k6/test.js
+++ product-similarity-service/shared/k6/test.js
@@ -45,7 +45,8 @@ export let options = {
   }
 };
 
-const host = "http://host.docker.internal:5000";
+const host = "http://app:5000";
+const influx = "http://influxdb:8086";
 
 export function normal() {
   http.get(host + "/product/1/similar");
